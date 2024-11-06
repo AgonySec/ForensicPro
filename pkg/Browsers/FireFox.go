9db147a4e71816cdcfc5c61f83b4cc4fd346dcbc
AgonySec <chenyuanhang2022@qq.com>
2024-11-07 02:26:08 +0800

    完成搜狗浏览器取证功能，补充了一些firefox浏览器取证文件记录

--- pkg/Browsers/FireFox.go
+++ pkg/Browsers/FireFox.go
@@ -3,6 +3,7 @@ package Browsers
 import (
 	"ForensicPro/utils"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -135,7 +136,49 @@ func FirefoxSave(path string) {
 		}
 
 	}
+	// 获取目录列表
+	directories, err := ioutil.ReadDir(FirefoxPath)
+	if err != nil {
+		fmt.Printf("读取目录时出错: %v\n", err)
+		return
+	}
 
+	// 遍历目录
+	for _, dir := range directories {
+		path2 := filepath.Join(FirefoxPath, dir.Name())
+
+		// 检查 storage-sync-v2.sqlite 文件是否存在
+		sqlitePath := filepath.Join(path2, "storage-sync-v2.sqlite")
+		if _, err := os.Stat(sqlitePath); err == nil {
+			// 复制 storage-sync-v2.sqlite 文件
+			destSqlitePath := filepath.Join(targetDir, "storage-sync-v2.sqlite")
+			if err := utils.CopyFile(sqlitePath, destSqlitePath); err != nil {
+				fmt.Printf("复制文件时出错: %v\n", err)
+				continue
+			}
+
+			// 检查并复制 storage-sync-v2.sqlite-shm 文件
+			shmPath := filepath.Join(path2, "storage-sync-v2.sqlite-shm")
+			if _, err := os.Stat(shmPath); err == nil {
+				destShmPath := filepath.Join(targetDir, "storage-sync-v2.sqlite-shm")
+				if err := utils.CopyFile(shmPath, destShmPath); err != nil {
+					fmt.Printf("复制文件时出错: %v\n", err)
+				}
+			}
+
+			// 检查并复制 storage-sync-v2.sqlite-wal 文件
+			walPath := filepath.Join(path2, "storage-sync-v2.sqlite-wal")
+			if _, err := os.Stat(walPath); err == nil {
+				destWalPath := filepath.Join(targetDir, "storage-sync-v2.sqlite-wal")
+				if err := utils.CopyFile(walPath, destWalPath); err != nil {
+					fmt.Printf("复制文件时出错: %v\n", err)
+				}
+			}
+
+			// 找到并复制了所需的文件，跳出循环
+			break
+		}
+	}
 	fmt.Println("Firefox浏览器取证结束")
 
 }
--- pkg/Browsers/OldSogou.go
+++ pkg/Browsers/OldSogou.go
@@ -1,1 +1,155 @@
 package Browsers
+
+import (
+	"ForensicPro/utils"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var SogouName = "OldSogou"
+var SogouPath = GetOperaPath("SogouExplorer")
+var sougoDefaultPath = filepath.Join(SogouPath, "\\Webkit\\Default")
+
+func SogouCookies() string {
+	var builder strings.Builder
+	sougoMasterKeyPath := filepath.Join(SogouPath, "\\Webkit")
+	sougoMasterkey, _ := utils.GetMasterKey(sougoMasterKeyPath)
+	if sougoMasterkey == nil {
+		return ""
+	}
+	text := filepath.Join(sougoDefaultPath, "Cookies")
+	if _, err := os.Stat(text); os.IsNotExist(err) {
+		return ""
+	}
+	// 创建临时文件
+	tempFileName, err := os.CreateTemp("", "temp-*.db")
+	if err != nil {
+		return ""
+	}
+	defer os.Remove(tempFileName.Name()) // 确保临时文件在函数结束时被删除
+
+	// 复制文件
+	if err := utils.CopyFile(text, tempFileName.Name()); err != nil {
+		return ""
+	}
+	// 连接 SQLite 数据库
+	db, err := sql.Open("sqlite3", tempFileName.Name())
+	if err != nil {
+		return ""
+	}
+	defer db.Close()
+
+	// 查询 UserRankUrl 表
+	rows, err := db.Query("SELECT host_key,name,encrypted_value FROM cookies")
+	if err != nil {
+		return ""
+	}
+	defer rows.Close()
+	// 遍历查询结果
+	for rows.Next() {
+		//var hostKey, name, encryptedValue string
+		var hostKey, name string
+		var encryptedValue []byte
+		if err := rows.Scan(&hostKey, &name, &encryptedValue); err != nil {
+			return ""
+		}
+
+		// 解密 encrypted_value
+		decryptedValue, err := utils.DecryptAESGCM(encryptedValue, sougoMasterkey)
+		if err != nil {
+			continue
+		}
+
+		// 构建输出字符串
+		builder.WriteString(fmt.Sprintf("[%s] \t {%s}={%s}\n", hostKey, name, decryptedValue))
+	}
+
+	if err := rows.Err(); err != nil {
+		return ""
+	}
+	return builder.String()
+}
+func SogouHistory() string {
+	var builder strings.Builder
+	// 构建 HistoryUrl3.db 的路径
+	text := filepath.Join(SogouPath, "HistoryUrl3.db")
+
+	// 检查文件是否存在
+	if _, err := os.Stat(text); os.IsNotExist(err) {
+		return ""
+	}
+
+	// 创建临时文件
+	tempFileName, err := os.CreateTemp("", "temp-*.db")
+	if err != nil {
+		return ""
+	}
+	defer os.Remove(tempFileName.Name()) // 确保临时文件在函数结束时被删除
+
+	// 复制文件
+	if err := utils.CopyFile(text, tempFileName.Name()); err != nil {
+		return ""
+	}
+	// 连接 SQLite 数据库
+	db, err := sql.Open("sqlite3", tempFileName.Name())
+	if err != nil {
+		return ""
+	}
+	defer db.Close()
+
+	// 查询 UserRankUrl 表
+	rows, err := db.Query("SELECT id FROM UserRankUrl")
+	if err != nil {
+		return ""
+	}
+	defer rows.Close()
+	// 遍历查询结果
+	for rows.Next() {
+		var id string
+		if err := rows.Scan(&id); err != nil {
+			return ""
+		}
+		builder.WriteString(id)
+		builder.WriteString("\n")
+	}
+
+	if err := rows.Err(); err != nil {
+		return ""
+	}
+
+	return builder.String()
+
+}
+
+func SogouSave(path string) {
+
+	if _, err := os.Stat(SogouPath); err == nil {
+		sogouHistory := SogouHistory()
+		sogouCookies := SogouCookies()
+		targetPath := filepath.Join(path, SogouName)
+		if err := os.MkdirAll(targetPath, os.ModePerm); err != nil {
+			log.Fatalf("创建目录失败: %v", err)
+		}
+		if sogouHistory != "" {
+			outputFile := SogouName + "_history.txt"
+			utils.WriteToFile(sogouHistory, targetPath+"\\"+outputFile)
+		}
+		if sogouCookies != "" {
+			outputFile := SogouName + "_cookies.txt"
+			utils.WriteToFile(sogouCookies, targetPath+"\\"+outputFile)
+		}
+		// 检查并复制Local Storage文件
+		sougoLSPath := filepath.Join(sougoDefaultPath, "Local Storage")
+		if _, err := os.Stat(sougoLSPath); err == nil {
+			destPath := filepath.Join(targetPath, "Local Storage")
+			if err := utils.CopyDirectory(sougoLSPath, destPath); err != nil {
+				fmt.Printf("复制文件时出错: %v\n", err)
+			}
+		}
+	}
+	fmt.Println(SogouName + " 浏览器信息已保存")
+}
