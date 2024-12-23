49b2af20ee44c09c624eda0cb03b6e15fff26b01
AgonySec <chenyuanhang2022@qq.com>
2024-12-23 11:22:41 +0800

    修复bug，优化代码结构，增加线程超时时间

--- cmd/forensicpro/main.go
+++ cmd/forensicpro/main.go
@@ -27,62 +27,59 @@ func main() {
 	elapsedTime := endTime.Sub(startTime) // 计算程序运行时间
 	fmt.Println("=================Windows取证结束=================")
 	fmt.Printf("程序运行时间: %v\n", elapsedTime)
-
+	// 提示用户按任意键继续
+	fmt.Println("按任意键继续...")
+	fmt.Scanln()
 }
-
 func GetAll(path string) {
-
 	var wg sync.WaitGroup
 
+	// 设置超时时间
+	timeout := 2 * time.Minute
+
 	// Browsers
-	wg.Add(4)
-	go func() { Browsers.ChromeSave(path); wg.Done() }()
-	go func() { Browsers.FirefoxSave(path); wg.Done() }()
-	go func() { Browsers.IESave(path); wg.Done() }()
-	go func() { Browsers.SogouSave(path); wg.Done() }()
+	runWithTimeout(&wg, timeout, func() { Browsers.ChromeSave(path) })
+	runWithTimeout(&wg, timeout, func() { Browsers.FirefoxSave(path) })
+	runWithTimeout(&wg, timeout, func() { Browsers.IESave(path) })
+	runWithTimeout(&wg, timeout, func() { Browsers.SogouSave(path) })
 
 	// Messengers
-	wg.Add(7)
-	go func() { Messengers.LineSave(path); wg.Done() }()
-	go func() { Messengers.QQSave(path); wg.Done() }()
-	go func() { Messengers.DiscordSave(path); wg.Done() }()
-	go func() { Messengers.TelegramSave(path); wg.Done() }()
-	go func() { Messengers.EnigmaSave(path); wg.Done() }()
-	go func() { Messengers.DingTalkSave(path); wg.Done() }()
-	go func() { Messengers.SkypeSave(path); wg.Done() }()
+	runWithTimeout(&wg, timeout, func() { Messengers.LineSave(path) })
+	runWithTimeout(&wg, timeout, func() { Messengers.QQSave(path) })
+	runWithTimeout(&wg, timeout, func() { Messengers.DiscordSave(path) })
+	runWithTimeout(&wg, timeout, func() { Messengers.TelegramSave(path) })
+	runWithTimeout(&wg, timeout, func() { Messengers.EnigmaSave(path) })
+	runWithTimeout(&wg, timeout, func() { Messengers.DingTalkSave(path) })
+	runWithTimeout(&wg, timeout, func() { Messengers.SkypeSave(path) })
 
 	// FTPS
-	wg.Add(3)
-	go func() { FTPS.FileZillaSave(path); wg.Done() }()
-	go func() { FTPS.SnowflakeSave(path); wg.Done() }()
-	go func() { FTPS.WinSCPSave(path); wg.Done() }()
+	runWithTimeout(&wg, timeout, func() { FTPS.FileZillaSave(path) })
+	runWithTimeout(&wg, timeout, func() { FTPS.SnowflakeSave(path) })
+	runWithTimeout(&wg, timeout, func() { FTPS.WinSCPSave(path) })
 
 	// Mails
-	wg.Add(4)
-	go func() { Mails.FoxmailSave(path); wg.Done() }()
-	go func() { Mails.MailBirdSave(path); wg.Done() }()
-	go func() { Mails.OutlookSave(path); wg.Done() }()
-	go func() { Mails.MailMasterSave(path); wg.Done() }()
+	runWithTimeout(&wg, timeout, func() { Mails.FoxmailSave(path) })
+	runWithTimeout(&wg, timeout, func() { Mails.MailBirdSave(path) })
+	runWithTimeout(&wg, timeout, func() { Mails.OutlookSave(path) })
+	runWithTimeout(&wg, timeout, func() { Mails.MailMasterSave(path) })
 
 	// SoftWares
-	wg.Add(8)
-	go func() { SoftWares.NeteaseCloudMusicSave(path); wg.Done() }()
-	go func() { SoftWares.NavicatSave(path); wg.Done() }()
-	go func() { SoftWares.VSCodeSave(path); wg.Done() }()
-	go func() { SoftWares.XmanagerSave(path); wg.Done() }()
-	go func() { SoftWares.FinalShellSave(path); wg.Done() }()
-	go func() { SoftWares.SQLyogSave(path); wg.Done() }()
-	go func() { SoftWares.SecureCRTSave(path); wg.Done() }()
-	go func() { SoftWares.DBeaverSave(path); wg.Done() }()
+	runWithTimeout(&wg, timeout, func() { SoftWares.NeteaseCloudMusicSave(path) })
+	runWithTimeout(&wg, timeout, func() { SoftWares.NavicatSave(path) })
+	runWithTimeout(&wg, timeout, func() { SoftWares.VSCodeSave(path) })
+	runWithTimeout(&wg, timeout, func() { SoftWares.XmanagerSave(path) })
+	runWithTimeout(&wg, timeout, func() { SoftWares.FinalShellSave(path) })
+	runWithTimeout(&wg, timeout, func() { SoftWares.SQLyogSave(path) })
+	runWithTimeout(&wg, timeout, func() { SoftWares.SecureCRTSave(path) })
+	runWithTimeout(&wg, timeout, func() { SoftWares.DBeaverSave(path) })
 
 	// SystemInfos
-	wg.Add(6)
-	go func() { SystemInfos.ScreenShotInfoSave(path); wg.Done() }()
-	go func() { SystemInfos.WifiInfoSave(path); wg.Done() }()
-	go func() { SystemInfos.InstalledAppSave(path); wg.Done() }()
-	go func() { SystemInfos.SystemInfoSave(path); wg.Done() }()
-	go func() { SystemInfos.RegEditSave(path); wg.Done() }()
-	go func() { SystemInfos.WindowsLogsSave(path); wg.Done() }()
+	runWithTimeout(&wg, timeout, func() { SystemInfos.InstalledAppSave(path) })
+	runWithTimeout(&wg, timeout, func() { SystemInfos.SystemInfoSave(path) })
+	runWithTimeout(&wg, timeout, func() { SystemInfos.RegEditSave(path) })
+	runWithTimeout(&wg, timeout, func() { SystemInfos.WindowsLogsSave(path) })
+	runWithTimeout(&wg, timeout, func() { SystemInfos.ScreenShotInfoSave(path) })
+	runWithTimeout(&wg, timeout, func() { SystemInfos.WifiInfoSave(path) })
 
 	wg.Wait()
 	fmt.Println("已完成全部取证，压缩文件中：")
@@ -90,5 +87,22 @@ func GetAll(path string) {
 	if err != nil {
 		return
 	}
+}
 
+func runWithTimeout(wg *sync.WaitGroup, timeout time.Duration, f func()) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		done := make(chan bool)
+		go func() {
+			f()
+			done <- true
+		}()
+		select {
+		case <-done:
+			// 函数执行完成
+		case <-time.After(timeout):
+			fmt.Println("函数执行超时")
+		}
+	}()
 }
